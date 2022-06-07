59865111efa46831d873b6e0f7ee7f672850f1a4
r320 <zz1236zz@naver.com>
2022-06-07 23:52:23 +0900

    tmp

--- detections/ObjectDetector.py
+++ detections/ObjectDetector.py
@@ -1,2 +1,22 @@
+import jetson.inference
+import jetson.utils
+
+import argparse
+import sys
 
 
+class ObjectDetector:
+    
+    def __init__(self, network="ssd-mobilenet-v2", threshold=0.5, overlay="box,conf") -> None:
+        self.net = jetson.inference.detectNet(network, sys.argv, threshold)
+        self.overlay = overlay
+
+    def __call__(self, img):
+        detections = self.net.Detect(img, overlay=self.overlay)
+
+        # print("detected {:d} objects in image".format(len(detections)))
+
+        # for detection in detections:
+        #     print(detection)
+
+        # output.Render(img)
--- main.py
+++ main.py
@@ -1,44 +1,71 @@
+import jetson.inference
+import jetson.utils
+
+import sys
 import cv2
+
 from detections.LaneDetector import LaneDetector
+from detections.ObjectDetector import ObjectDetector
 
-
+import time
 
 def main():
-    video_name = "/Users/joono/Desktop/joono/ComputerVisionADASProject/videos/highway_D6_Trim.mp4"
+    video_name = "/home/r320/ComputerVisionADASProject/videos/highway_D5_Trim.mp4"
     fps = 30
 
     capfile = f'filesrc location={video_name} ! qtdemux ! queue \
-                            ! h264parse ! omxh264dec ! nvvidconv ! video/x-raw,format=BGRx,width=512,height=256 \
+                            ! h264parse ! omxh264dec ! nvvidconv ! video/x-raw,format=BGRx,width=720,height=380 \
                             ! videorate ! video/x-raw,framerate={fps}/1 !queue ! videoconvert ! queue ! video/x-raw, format=BGR \
                             ! appsink'
 
-    cap = cv2.VideoCapture(capfile, cv2.CAP_GSTREAMER)
+    capfile2 = f'filesrc location={video_name} ! qtdemux ! queue \
+                            ! h264parse ! omxh264dec ! nvvidconv ! video/x-raw,format=BGRx ! queue \
+                            ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink'
 
-    cap = cv2.VideoCapture(video_name)
+    # cap = cv2.VideoCapture(capfile2, cv2.CAP_GSTREAMER)
 
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # cap = cv2.VideoCapture(video_name)
+    cap = jetson.utils.videoSource(video_name, argv=sys.argv) 
+    # output = jetson.utils.videoOutput("", argv=sys.argv)
 
-    print(w, h)
+
+    # w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    # h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    w, h = 1920, 1080
+
+
+    # print(w, h)
 
     laneDetector = LaneDetector(video_name)
+    objectDetector = ObjectDetector()
 
+    ret = True
     while True:
-        ret, frame = cap.read()
+        # ret, frame = cap.read()
+        img = cap.Capture()
         if not ret:
             break
 
+        tt = time.perf_counter()
+
+        frame = jetson.utils.cudaToNumpy(img)
         template = frame[round(h*(1/3)):, :, :]
-        cv2.imshow("half image", template)
+        
+        # cv2.imshow("half image", template)
 
         template = cv2.resize(template, (256, 128))
-        gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-        hsv = cv2.cvtColor(template, cv2.COLOR_BGR2HSV)
+        gray = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY)
+        hsv = cv2.cvtColor(template, cv2.COLOR_RGB2HSV)
 
         laneDetector(gray, hsv, template)
         laneDetector.show_BEV()
 
-        cv2.imshow("template", template)
+        # objectDetector(img)
+
+        tt2 = time.perf_counter()
+        print(f"FPS {1000 * (tt2-tt):.3f}ms")
+
+        # cv2.imshow("template", template)
 
         k = cv2.waitKey(30)
         if 27 == k:
