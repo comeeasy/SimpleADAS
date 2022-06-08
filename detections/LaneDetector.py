e0e36f8580128095ed112abddd19c61cb67a48e9
r320 <zz1236zz@naver.com>
2022-06-08 18:43:03 +0900

    feat: roi lane detection

--- detections/LaneDetector.py
+++ detections/LaneDetector.py
@@ -98,9 +98,11 @@ class LaneDetector:
     BEV_WIDTH = 32
     BEV_HEIGHT = 128
 
-    N_WINDOWS = 32
+    N_WINDOWS = 16
     window_height = BEV_HEIGHT // N_WINDOWS
     window_width = 10
+
+    ROI_WIDTH = 2
 
     BEV_POINTS = [
         [0, 0],
@@ -124,7 +126,7 @@ class LaneDetector:
             self.LANE_ROI_POINTS = [
                 [self.WARPAFFINE_WIDTH // 2 - 30, 20],
                 [30, self.WARPAFFINE_HEIGHT],
-                [self.WARPAFFINE_WIDTH // 2 + 30, 20],
+                [self.WARPAFFINE_WIDTH // 2 + 20, 20],
                 [self.WARPAFFINE_WIDTH - 30, self.WARPAFFINE_HEIGHT],
             ]
 
@@ -136,23 +138,32 @@ class LaneDetector:
         self.inv_M = np.linalg.inv(self.M)
         self.BEV2TEMPLATE_LOOKUPTBL = []
         # HSV yello
-        self.low_yellow = np.array([10, 100, 100])
-        self.upper_yellow = np.array([52, 255, 255])
+        self.low_yellow = np.array([0, 50, 0])
+        self.upper_yellow = np.array([120, 255, 255])
 
-        for x in tqdm(range(self.BEV_WIDTH)):
+        for x in tqdm(range(self.BEV_WIDTH+1)):
             self.BEV2TEMPLATE_LOOKUPTBL.append([])
-            for y in range(self.BEV_HEIGHT):
+            for y in range(self.BEV_HEIGHT+1):
                 bef_coor = np.array([x, y, 1])
                 aft_coor = np.matmul(self.inv_M, np.transpose(bef_coor))
 
                 self.BEV2TEMPLATE_LOOKUPTBL[x].append(
-                    [
-                        np.array([int(aft_coor[0] * (1 / aft_coor[2])),
-                                  int(aft_coor[1] * (1 / aft_coor[2]))])
-                    ])
+                        [int(aft_coor[0] * (1 / aft_coor[2])),
+                         int(aft_coor[1] * (1 / aft_coor[2]))]
+                        )
+                    
 
         self.BEV_color = np.zeros((self.BEV_HEIGHT, self.BEV_WIDTH, 3))
         self.closing_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        self.roi_to_BEV_table = []
+        for row in range(self.N_WINDOWS+1):
+            _ = list()
+            _row = 8 * row
+            for l_idx in range(0, self.BEV_WIDTH // 2):
+                _.append((2 * l_idx, _row))
+            for r_idx in range(self.BEV_WIDTH // 2, self.BEV_WIDTH+1):
+                _.append((2 * r_idx + 1, _row))
+            self.roi_to_BEV_table.append(_)
 
     def right_line_detect(self, img, template):
         X, Y = [], []
@@ -168,16 +179,18 @@ class LaneDetector:
             except:
                 pass
 
+        
         if len(X) > 1:
             # self.lr.fit(np.array(X).reshape(-1, 1), np.array(Y))
             try:
                 z = np.polyfit(X, Y, 1)
             except:
                 return None
+
             p = np.poly1d(z)
             # print(p)
 
-            if abs(p.c[0]) < 20:
+            if abs(p.c[0]) < 12:
                 return None
 
             right_points = []
@@ -188,7 +201,7 @@ class LaneDetector:
                     continue
 
                 right_points.append(self.BEV2TEMPLATE_LOOKUPTBL[x][y])
-                # cv2.circle(self.BEV_color, (x, y), 5, (0, 255, 255), -1)
+                cv2.circle(self.BEV_color, (x, y), 5, (0, 255, 255), -1)
 
             x, y = int((self.BEV_HEIGHT - p.c[1]) / p.c[0]), self.BEV_HEIGHT-1
             print(x, y)
@@ -222,6 +235,7 @@ class LaneDetector:
                 z = np.polyfit(X, Y, 1)
             except:
                 return None
+
             p = np.poly1d(z)
 
             if abs(p.c[0]) < 20:
@@ -235,7 +249,7 @@ class LaneDetector:
                     continue
 
                 left_points.append(self.BEV2TEMPLATE_LOOKUPTBL[x][y])
-                # cv2.circle(self.BEV_color, (x, y), 5, (0, 255, 255), -1)
+                cv2.circle(self.BEV_color, (x, y), 5, (0, 255, 255), -1)
 
             x, y = int((self.BEV_HEIGHT - p.c[1]) / p.c[0]), self.BEV_HEIGHT - 1
             if 0 <= x < self.BEV_WIDTH // 2:
@@ -248,8 +262,61 @@ class LaneDetector:
             else:
                 return None
 
+    def roi_lane_detect(self, roi_points, template=None):
+        l_x, r_x = 0, 0
+        l_pts, r_pts = [], []
+
+        # num of rois in a window
+        n_rois = self.window_width // self.ROI_WIDTH
+        # total num of roi in a row
+        roi_width = self.BEV_WIDTH // self.ROI_WIDTH
+
+        # find most inner lane
+        for i in range(self.N_WINDOWS): # 16: N_WINDOW
+            for l_idx in range(n_rois - 1, -1, -1): # 3, 2, 1, 0
+                if roi_points[i][l_idx] > 0:
+                    x_bev, y_bev = self.roi_to_BEV_table[i][l_idx]
+                    l_pts.append(self.BEV2TEMPLATE_LOOKUPTBL[x_bev][y_bev])
+                    cv2.circle(self.BEV_color, self.roi_to_BEV_table[i][l_idx], 2, (0, 255, 255), -1)
+                    # last idx is left x
+                    l_x = l_idx
+                    break
+            
+            for r_idx in range(roi_width - n_rois, roi_width): # 13, 12, 11, 10:
+                if roi_points[i][r_idx] > 0:
+                    x_bev, y_bev = self.roi_to_BEV_table[i][r_idx]
+                    r_pts.append(self.BEV2TEMPLATE_LOOKUPTBL[x_bev][y_bev])
+
+                    cv2.circle(self.BEV_color, self.roi_to_BEV_table[i][r_idx], 2, (0, 255, 255), -1)
+                    # last idx is right x
+                    r_x = r_idx
+                    break
+
+        # two short lane has length of 3points (two short lane is a line)
+        if len(l_pts) > 4:
+            x_bev, y_bev = self.roi_to_BEV_table[self.N_WINDOWS][l_x]
+            pts = self.BEV2TEMPLATE_LOOKUPTBL[x_bev][y_bev]
+            l_pts.append(pts)
+            l_x = pts[0]
+        else:
+            l_x = None
+
+        if len(r_pts) > 4:
+            x_bev, y_bev = self.roi_to_BEV_table[self.N_WINDOWS][r_x]
+            pts = self.BEV2TEMPLATE_LOOKUPTBL[x_bev][y_bev]
+            r_pts.append(pts)
+            r_x = pts[0]
+        else:
+            r_x = None
+
+        if not template is None:
+            cv2.polylines(template, [np.array(l_pts)], False, (255, 0 ,255), 2)
+            cv2.polylines(template, [np.array(r_pts)], False, (255, 0 ,255), 2)
+
+        return l_x, r_x
+
     def __call__(self, gray, hsv, template=None):
-        # self.BEV_color = np.zeros_like(self.BEV_color)
+        self.BEV_color = np.zeros_like(self.BEV_color)
 
         # edges = cv2.GaussianBlur(gray, (15, 15), sigmaX=3, sigmaY=3)
         # edges = cv2.Canny(edges, 50, 70)
@@ -266,14 +333,19 @@ class LaneDetector:
         out = cv2.add(thresh, center_line_mask)
         # out = cv2.add(out, edges)
 
-
         # BEV image (256, 1024) (W, H)
         out = cv2.warpPerspective(out, self.M, (self.BEV_WIDTH, self.BEV_HEIGHT))
         out = cv2.normalize(out, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
 
-        # cv2.imshow("lane middle result", out)
+        # roi points temp 
+        roi_points = cv2.resize(out, (self.BEV_WIDTH // self.ROI_WIDTH, self.N_WINDOWS), interpolation=cv2.INTER_NEAREST)
+        l_x, r_x = self.roi_lane_detect(roi_points, template)
+        
+        cv2.imshow("lane middle result", out)
 
-        return self.left_line_detect(out, template), self.right_line_detect(out, template)
+        return l_x, r_x        
+
+        # return self.left_line_detect(out, template), self.right_line_detect(out, template)
 
     def show_BEV(self):
         cv2.imshow("BEV", self.BEV_color)
